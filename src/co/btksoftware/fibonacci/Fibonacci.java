77cae05d6c9d370109deb13dd5b65cf0d7aeb3e5
Tóth Kristóf <tothkrisi@gmail.com>
2018-01-09 15:08:06 +0100

    Simplified Fibonacci sequence algorithm

--- src/co/btksoftware/fibonacci/Fibonacci.java
+++ src/co/btksoftware/fibonacci/Fibonacci.java
@@ -13,12 +13,10 @@ public class Fibonacci {
     }
 
     private static int Fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
+        if (n > 1) {
             return Fib(n - 1) + Fib(n - 2);
+        } else {
+            return n;
         }
     }
 }
