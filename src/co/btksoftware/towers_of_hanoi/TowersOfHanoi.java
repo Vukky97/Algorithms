63c6fc24d7030ee38795beaff4f14cfd4c5ad01c
Tóth Kristóf <tothkrisi@gmail.com>
2018-01-09 14:34:28 +0100

    Added Towers of Hanoi problem

---
+++ src/co/btksoftware/towers_of_hanoi/TowersOfHanoi.java
@@ -0,0 +1,18 @@
+package co.btksoftware.towers_of_hanoi;
+
+public class TowersOfHanoi {
+
+    public static void main(String[] args) {
+        // we're moving the disks from rod 1 to rod 3,
+        // while the second rod is the helper
+        Hanoi(5, "1. rod", "3. rod", "2. rod");
+    }
+
+    private static void Hanoi(int N, String source, String destination, String helper) {
+        if (N > 0) {
+            Hanoi(N - 1, source, helper, destination);
+            System.out.println(N + ". moving disk " + source + " -> " + destination);
+            Hanoi(N - 1, helper, destination, source);
+        }
+    }
+}
