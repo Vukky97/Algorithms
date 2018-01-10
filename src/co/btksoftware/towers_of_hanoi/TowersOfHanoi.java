bc19cacad92cc5adeca7a7a0655749b57a06cd6a
Tóth Kristóf <tothkrisi@gmail.com>
2018-01-10 16:41:23 +0100

    Reduced the number of disks

--- src/co/btksoftware/towers_of_hanoi/TowersOfHanoi.java
+++ src/co/btksoftware/towers_of_hanoi/TowersOfHanoi.java
@@ -5,7 +5,7 @@ public class TowersOfHanoi {
     public static void main(String[] args) {
         // we're moving the disks from rod 1 to rod 3,
         // while the second rod is the helper
-        Hanoi(5, "1. rod", "3. rod", "2. rod");
+        Hanoi(3, "1. rod", "3. rod", "2. rod");
     }
 
     private static void Hanoi(int N, String source, String destination, String helper) {
